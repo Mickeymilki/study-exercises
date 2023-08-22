9eacf3af2853bac047caa97dad88ed17dcf3d2f2
Mickeymilki <mickeymilki@yandex.ru>
2023-08-22 22:09:10 +0800

    2

--- 1.2/04.08_turtle.py
+++ 1.2/04.08_turtle.py
@@ -1,30 +1,27 @@
 import turtle
 from turtle import *
 
-screen = Screen()
-print(screen.screensize()) 
-
+scrn = Screen()
+scrn.screensize(canvwidth=500, canvheight=500,
+                  bg="lightgrey")
 t = turtle.Turtle()
-
 
 t.width(100)
 t.speed(10)
 
-t.color('red')
-t.pensize(7)
+t.color('black')
+t.pensize(5)
 
 for _ in range(1):
-    t.circle(-20)
-    t.forward(100)
-    t.circle(-20)
-
+ t.circle(-20)
+ t.forward(100)
+ t.circle(-20)
 
 t.bk(20)
 t.right(90)
 t.forward(200)
 
-
-# Колонна
+#Колонна
 t.left(180)
 t.goto(80,0)
 t.left(90)
@@ -44,20 +41,52 @@ t.circle(20)
 t.forward(100)
 t.circle(20)
 
-# Плющ
-t.color('green')
-t.pensize(5)
-
-t.penup()
-t.goto(80,-160)
-t.down()
+#Арка
 t.left(180)
-t.circle(-100,40)
+t.forward(120)
+t.right(90)
+t.forward(150)
+t.circle(70,180)
 
-for _ in range(2,5):
-    t.right(140)
-    t.circle(100,40)
-    t.left(140)
-    t.circle(-100,40)
+#Колонна левая
+t.forward(150)
+t.right(90)
+t.forward(20)
+t.circle(-20)
+
+t.forward(20)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(20)
+t.circle(-20)
+
+t.left(180)
+t.forward(50)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(30)
+t.left(180)
+t.forward(60)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(30)
+t.left(180)
+t.forward(50)
+t.circle(20)
+t.right(180)
+t.forward(20)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(20)
+t.circle(-20)
+t.forward(20)
+t.left(90)
+
+    
+    
 
 turtle.done()
